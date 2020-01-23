853b961f61d3ed715f02849958782d5dd87a3153
colombie <adrien.colombier@epitech.eu>
2020-01-22 19:38:56 -0500

    [MODIFIED] - Profile modified into 2 part with pro and fun part

--- website/src/app/components/profile/profile.component.ts
+++ website/src/app/components/profile/profile.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  quotes: Array<string> = [
+    '"If you don\'t control pawns, you are one."',
+    '"Do you have a longing? Would you like one?"',
+    '"Secrets are sharper than blades."',
+    '"Every problem is an opportunity."',
+    '"I like to leave a lasting impression."',
+    '"Wealth is valuable only as a means to power."',
+    '"Come, look me in the eyes."'
+  ];
+  quote: string = this.quotes[Math.floor(Math.random() * Math.floor(7))]
   constructor() { }
 
   ngOnInit() {
